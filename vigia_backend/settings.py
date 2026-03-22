c07892e55dafcb68f0db2b7650852a0f9ea8cea3
CarrilloCR <fabiancarrillo2k@gmail.com>
2026-03-22 16:51:03 -0600

    update settings for celery

--- vigia_backend/settings.py
+++ vigia_backend/settings.py
@@ -89,3 +89,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'America/Costa_Rica'
