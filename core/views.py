bf1875b70b6966c28cd0d577cff164fb264c786f
CarrilloCR <fabiancarrillo2k@gmail.com>
2026-03-24 14:06:27 -0600

    import timedelta

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.utils import timezone
+from datetime import timedelta
 from .models import (
     Clinica, Sede, Usuario, Medico, Paciente, Cita, Encuesta,
     RegistroKPI, Alerta, Notificacion, FeedbackAlerta,
